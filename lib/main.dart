6210146aa052fed4618f02f66b47795fb9195272
camargo <matheuscamarrgo@gmail.com>
2024-07-24 15:50:42 -0300

    Desafio 1

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,29 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Container(
-        color: Colors.white,
+        color: Colors.black,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
                   color: Colors.red,
-                  width: 100,
                   height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                ),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.blue,
                   width: 100,
-                  height: 100,
                 ),
                 Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
+                  color: Colors.amber,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.amberAccent,
+                  height: 100,
+                  width: 100,
                 ),
               ],
             ),
@@ -56,41 +47,44 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
+                  color: Colors.green,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
                   color: Colors.cyan,
-                  height: 50,
-                  width: 50,
+                  height: 100,
+                  width: 100,
                 ),
                 Container(
-                  color: Colors.pink,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.purple,
-                  height: 50,
-                  width: 50,
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
                 ),
               ],
             ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Texto',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.purple,
+                  height: 100,
+                  width: 100,
                 ),
-                textAlign: TextAlign.center,
-              ),
+                Container(
+                  color: Colors.pink,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  width: 100,
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                print("APertou botao");
-              },
-              child: Text("APerte aqui"),
-            ),
+
           ],
         ),
       ),
