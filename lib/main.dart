5dc5265ae9a64c23f941ac197660655bcfef7626
camargo <matheuscamarrgo@gmail.com>
2024-09-06 15:59:47 -0300

    flutter_ctrl_nav_est

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formacao_flutter/screens/form_screen.dart';
 import 'package:formacao_flutter/screens/initial_sreen.dart';
 
 void main() {
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const InicialScreen(),
+      //home: const InicialScreen(),
+      home: const FormScreen(),
     );
   }
 }
